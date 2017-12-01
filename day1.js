9b511cf9c57f5d70122d6c361e10d37f74143e25
Alex Kohen <alex.kohen@skyscanner.net>
2017-12-01 15:59:23 +0000

    Day 1

---
+++ day1.js
@@ -0,0 +1,24 @@
+fs = require('fs');
+
+function day1(input) {
+    let result = 0
+    let result_part2 = 0
+    const len = input.length
+
+    for ( let i=0; i<input.length; i++) {
+        const current = input[i]
+        const next = input[(i+1)%len]
+        const halfway = input[(i+len/2)%len]
+
+        if ( current == next ) {
+            result += Number(current)
+        }
+        if ( current == halfway ) {
+            result_part2 += Number(current)
+        }
+    }
+    return [result, result_part2]
+}
+
+
+fs.readFile('day1/input','utf-8', (err, data) => { console.log(day1(data)) });
