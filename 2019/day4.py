e90cd92c45c1bf6e28a7b07da047579c2402db65
Alexandre Kohen <akohen@scaleway.com>
2019-12-04 11:44:41 +0100

    Day4 keep part one result

--- 2019/day4.py
+++ 2019/day4.py
@@ -1,4 +1,4 @@
-def is_password_valid(pwd):
+def is_password_valid(pwd, part2=False):
   previous = 10
   seen = [0] * 10
   while(pwd > 0):
@@ -9,11 +9,17 @@ def is_password_valid(pwd):
     else:
       previous = n
       seen[n] += 1
-  return (2 in seen)
+  if part2:
+    return (2 in seen)
+  else:
+    return (seen.count(0) > 4)
 
 if __name__ == "__main__":
-  sum = 0
+  (sumA, sumB) = (0,0)
   for i in range(183564,657474):
-    if is_password_valid(i):
-      sum += 1
-  print(sum)
+    if is_password_valid(i, True):
+      sumB += 1
+    elif is_password_valid(i):
+      sumA += 1
+  print('Part one count: {}'.format(sumA+sumB))
+  print('Part two count: {}'.format(sumB))
