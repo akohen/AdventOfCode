f100d8cd24ec74d7f833872506d057ebff607ea4
Alexandre Kohen <alexandre@kohen.fr>
2019-12-05 15:49:06 +0100

    Day5 read/write + step 1 results

--- 2019/day5.py
+++ 2019/day5.py
@@ -4,80 +4,70 @@ class intcode_computer:
   
   def __init__(self, program, input_values=[]):
     self.pointer = 0
-    self.program = program
+    self.program = list(program)
     self.opcodes = {
       1: self.addition, 2: self.multiplication, 
       3: self.input, 4: self.output, 
       5: self.jump(operator.ne), 6: self.jump(operator.eq),
       7: self.comp(operator.lt), 8: self.comp(operator.eq),
-      99: lambda _: False}
+      99: lambda _: True}
     self.input_values = input_values
     self.output_values = []
 
   def __repr__(self):
-    return '\n'.join([str(e) for e in self.output_values]) 
-
-  def get(self, value, mode):
+    return '\n'.join([str(e) for e in self.output_values])
+  
+  def read(self, mode='1'):
+    value = self.program[self.pointer]
+    self.pointer += 1
     if(mode == '0'): return self.program[value]
     elif(mode == '1'): return value
     else: raise Exception('Unknown parameter mode')
-  
-  def current(self):
-    val = self.program[self.pointer]
-    self.pointer += 1
-    return val
+
+  def write(self, value):
+    self.program[self.read()] = value
   
   def parse_instruction(self, instr): # returns (opcode, modes)
     return (instr % 100, str(instr // 100).zfill(3)[::-1])
 
   def addition(self, modes):
-    value = self.get(self.current(), modes[0]) + self.get(self.current(), modes[1])
-    self.program[ self.current() ] = value
-    return True
+    self.write(self.read(modes[0]) + self.read(modes[1]))
 
   def multiplication(self, modes):
-    value = self.get(self.current(), modes[0]) * self.get(self.current(), modes[1])
-    self.program[ self.current() ] = value
-    return True
+    self.write(self.read(modes[0]) * self.read(modes[1]))
 
   def input(self, modes):
-    self.program[ self.current() ] = self.input_values.pop()
-    return True
+    self.write(self.input_values.pop())
 
   def output(self, modes):
-    self.output_values.append(self.get(self.current(), modes[0]))
-    return True
+    self.output_values.append(self.read(modes[0]))
 
-  def jump(self, op):
+  def jump(self, op): # jumps to arg1 if op(arg0,0) is true
     def _jump(modes):
-      if op(self.get(self.current(), modes[0]), 0):
-        self.pointer = self.get(self.current(), modes[1])
-      else: self.current()
-      return True
+      if op(self.read(modes[0]), 0):
+        self.pointer = self.read(modes[1])
+      else: self.pointer += 1
     return _jump
     
-  def comp(self, op):
+  def comp(self, op): # writes 1/0 if op(arg0, arg1) is true/false
     def _comp(modes):
-      if op(self.get(self.current(), modes[0]), self.get(self.current(), modes[1])):
-        self.program[ self.current() ] = 1
+      if op(self.read(modes[0]), self.read(modes[1])):
+        self.write(1)
       else:
-        self.program[ self.current() ] = 0
-      return True
+        self.write(0)
     return _comp
 
   def execute(self, debug=False):
-    while self.step(debug): pass
-    return True
+    while self.step(debug) is not True: pass
+    return self
 
   def step(self, debug=False):
-    (opcode, modes) = self.parse_instruction(self.current())
+    (opcode, modes) = self.parse_instruction(self.read())
     if debug: print(self.pointer, opcode, self.program)
     return self.opcodes[opcode](modes)
 
 if __name__ == "__main__":
   with open('./input/day5') as f:
     program = [int(i) for i in next(f).split(',')]
-    comp = intcode_computer(program,[5])
-    if comp.execute():
-      print(comp)
-    
+    print('Step 1 : {}'.format(intcode_computer(program,[1]).execute().output_values[-1]))
+    print('Step 2 : {}'.format(intcode_computer(program,[5]).execute()))
