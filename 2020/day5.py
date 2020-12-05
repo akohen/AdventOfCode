980c942ec9f44d50c8f6b75b6f16b8c325e28fdf
Alexandre Kohen <alexandre@kohen.fr>
2020-12-05 16:57:35 +0100

    2020 day5 cleanup

--- 2020/day5.py
+++ 2020/day5.py
@@ -1,24 +1,22 @@
 from pathlib import Path
-import sys
-import re
 
 
 def get_seats(boarding_passes):
     return [get_seat(p) for p in boarding_passes]
 
 def phase1(seats):
-    return max(seats,key=lambda x: x[2])[2]
+    return max(seats)[0]
 
 def get_seat(boarding_pass):
     row = int(boarding_pass[0:7].replace('F','0').replace('B',"1"),2)
     column = int(boarding_pass[7:10].replace('L','0').replace('R',"1"),2)
-    return row, column, row*8+column
+    return row*8+column, row, column
 
 def phase2(seats):
-    seats = sorted(seats, key=lambda x: x[2])
+    seats = sorted(seats)
     for i in range(len(seats)-1):
-        if seats[i+1][2] == seats[i][2]+2:
-            return seats[i][2]+1
+        if seats[i+1][0] == seats[i][0]+2:
+            return seats[i][0]+1
 
 if __name__ == "__main__":
     with Path(__file__).parent.joinpath("input/day5").open() as f:
