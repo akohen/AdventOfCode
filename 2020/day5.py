da2cd5665df264ad77fe349f11626e3b828d21e4
Alexandre Kohen <alexandre@kohen.fr>
2020-12-05 16:53:50 +0100

    2020 day5 part2

--- 2020/day5.py
+++ 2020/day5.py
@@ -15,16 +15,10 @@ def get_seat(boarding_pass):
     return row, column, row*8+column
 
 def phase2(seats):
-    max_id = phase1(seats)
-    max_row = -1
-    max_col = -1
-    for seat in seats:
-        max_row = max(max_row, seat[0])
-        max_col = max(max_col, seat[1])
-    for x in range(1, max_row+1):
-        for y in range(0, max_col+1):
-            if (x,y,x*8+y) not in seats and (x*8+y) < max_id:
-                return x*8+y
+    seats = sorted(seats, key=lambda x: x[2])
+    for i in range(len(seats)-1):
+        if seats[i+1][2] == seats[i][2]+2:
+            return seats[i][2]+1
 
 if __name__ == "__main__":
     with Path(__file__).parent.joinpath("input/day5").open() as f:
