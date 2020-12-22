2c9dcf0d12e6563e8fa37067d2491619676dfb6f
Alexandre Kohen <alexandre@kohen.fr>
2020-12-22 12:38:10 +0100

    2020 day21 cleanup

--- 2020/day21.py
+++ 2020/day21.py
@@ -4,53 +4,31 @@ import sys
 
 TEST_MODE = bool(len(sys.argv) > 1 and sys.argv[1] == "test")
 
-def phase1(data):
+def solve(data):
     ingredient_counts = defaultdict(int)
-    possible_ingredients = defaultdict(lambda: list(ingredient_counts.keys()))
+    possible_ingredients = defaultdict(lambda: set(ingredient_counts.keys()))
     for ings, allgs in data:
         for ing in ings:
             ingredient_counts[ing] += 1
     
     for ings, allgs in data:
         for allergen in allgs:
-            for ing in ingredient_counts:
-                if ing not in ings and ing in possible_ingredients[allergen]:
-                    possible_ingredients[allergen].remove(ing)
+            possible_ingredients[allergen] &= set(ings)
     
 
     allergens = {}
-    has_allergen = []
-    for _ in range(len(possible_ingredients)):
-        for allergen in possible_ingredients:
-            if len(possible_ingredients[allergen]) == 1:
-                ingredient = possible_ingredients[allergen].pop()
-                allergens[allergen] = ingredient
-                has_allergen.append(ingredient)
-                for other_allergen in possible_ingredients:
-                    if other_allergen != allergen and ingredient in possible_ingredients[other_allergen]:
-                        possible_ingredients[other_allergen].remove(ingredient)
-    
-    total = 0
-    for ingredient in ingredient_counts:
-        if ingredient not in has_allergen:
-            total += ingredient_counts[ingredient]
-    
-    canonical_list = ""
-    for allergen in sorted(allergens):
-        canonical_list += allergens[allergen]+","
-    return total, canonical_list[:-1]
+    while any(possible_ingredients.values()):
+        allergen, ingredient = next((a, possible_ingredients[a].pop()) for a in possible_ingredients if len(possible_ingredients[a]) == 1)
+        allergens[allergen] = ingredient
+        for other_allergen in possible_ingredients:
+            possible_ingredients[other_allergen].discard(ingredient)
 
-def load(data):
-    result = []
-    for line in data:
-        ingredients, allergens = line.split(" (contains ")
-        ingredients = ingredients.split(" ")
-        allergens = allergens[:-1].split(", ")
-        result.append([ingredients, allergens])
-    return result
+    total = sum([ingredient_counts[ingredient] for ingredient in ingredient_counts if ingredient not in allergens.values()])
+    canonical_list = ",".join([allergens[allergen] for allergen in sorted(allergens)])
+    return total, canonical_list
 
 if __name__ == "__main__":
     with Path(__file__).parent.joinpath("input/day21_sample" if TEST_MODE else "input/day21").open() as f:
-        DATA = load([line.strip() for line in f])
+        DATA = [[a[0].split(" "), a[1][:-1].split(", ")] for line in f if (a := line.strip().split(" (contains "))]
 
-        print('Phase 1: {}\nPhase 2: {}'.format(*phase1(DATA)))
+        print('Phase 1: {}\nPhase 2: {}'.format(*solve(DATA)))
