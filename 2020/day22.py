fca1491a021df6d9c970395f36d87fc80201f47b
Alexandre Kohen <alexandre@kohen.fr>
2020-12-22 11:41:59 +0100

    2020 day22 cleanup

--- 2020/day22.py
+++ 2020/day22.py
@@ -11,17 +11,12 @@ def get_score(p1, p2):
         total += card * (len(winner) - index)
     return total
 
-def phase1(p1, p2):
-    while len(p1) != 0 and len(p2) != 0:
-        c1, c2 = p1.popleft(), p2.popleft()
-        if c1 > c2:
-            p1.append(c1)
-            p1.append(c2)
-        else:
-            p2.append(c2)
-            p2.append(c1)
-    
-    return get_score(p1, p2)
+def phase1(players):
+    while len(players[0]) != 0 and len(players[1]) != 0:
+        c = players[0].popleft(), players[1].popleft()
+        winner = 0 if c[0] > c[1] else 1
+        players[winner] += [c[winner],c[winner^1]]
+    return get_score(*players)
 
 def hash(players):
     value = ""
@@ -46,12 +41,9 @@ def recursive_combat(players):
                 [deque(islice(players[0],0,c[0])), 
                 deque(islice(players[1],0,c[1]))]
             )[0]
-        elif c[0] > c[1]:
-            winner = 0
         else:
-            winner = 1
-        players[winner].append(c[winner])
-        players[winner].append(c[winner^1])
+            winner = 0 if c[0] > c[1] else 1
+        players[winner] += [c[winner],c[winner^1]]
     return winner, players
 
 def phase2(players):
@@ -59,14 +51,11 @@ def phase2(players):
     return get_score(*players)
 
 def load(data):
-    cards = deque()
-    for card in data[1:]:
-        cards.append(int(card))
-    return cards
+    return deque([int(card) for card in data[1:]])
 
 if __name__ == "__main__":
     with Path(__file__).parent.joinpath("input/day22_sample" if TEST_MODE else "input/day22").open() as f:
         P1, P2 = [load(line.split("\n")) for line in f.read().split("\n\n")]
 
-        print('Phase 1: {}'.format(phase1(P1.copy(), P2.copy())))
+        print('Phase 1: {}'.format(phase1([P1.copy(), P2.copy()])))
         print('Phase 2: {}'.format(phase2([P1, P2])))
