07bb19129cd014318543e3795ca5c1a0e4ede2c0
Alexandre Kohen <alexandre@kohen.fr>
2021-10-14 08:10:10 +0200

    2020 day 5 Removed test (not sure what this was supposed to test)

--- 2020/day5_test.py
+++ 2020/day5_test.py
@@ -6,9 +6,3 @@ def test_phase1():
         boarding_passes = [line.strip() for line in f]
         seats = day5.get_seats(boarding_passes)
         assert day5.phase1(seats) == 820
-
-
-def test_seat_ids():
-    seats = [("FBFBBFFRLR", 357), ("BFFFBBFRRR", 567), ("FFFBBBFRRR", 119), ("BBFFBBFRLL", 820)]
-    for s in seats:
-        assert day5.get_seat(s[0])[2] == s[1]
