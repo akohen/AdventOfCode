26cdb2607b1fdf68430eaafd9c6f8a153687bcb9
Alexandre Kohen <alexandre@kohen.fr>
2021-10-14 20:13:46 +0200

    2015 day 3

---
+++ 2015/day3.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+def phase1(values):
+    pos = (0,0)
+    seen = set([pos])
+    for val in values:
+        if val == '>': pos = (pos[0]+1,pos[1])
+        if val == '<': pos = (pos[0]-1,pos[1])
+        if val == '^': pos = (pos[0],pos[1]+1)
+        if val == 'v': pos = (pos[0],pos[1]-1)
+        seen.add(pos)
+    return len(seen)
+
+def phase2(values):
+    pos = [(0,0), (0,0)]
+    seen = set([(0,0)])
+    for i, val in enumerate(values):
+        active = i%2
+        if val == '>': pos[active] = (pos[active][0]+1,pos[active][1])
+        if val == '<': pos[active] = (pos[active][0]-1,pos[active][1])
+        if val == '^': pos[active] = (pos[active][0],pos[active][1]+1)
+        if val == 'v': pos[active] = (pos[active][0],pos[active][1]-1)
+        seen.add(pos[active])
+    return len(seen)
+
+if __name__ == "__main__":
+    with Path(__file__).parent.joinpath("input/day3").open(encoding="UTF-8") as f:
+        my_input = f.read()
+
+        print(f"Phase 1: {phase1(my_input)}")
+        print(f"Phase 2: {phase2(my_input)}")
---
+++ 2015/day3_test.py
@@ -0,0 +1,11 @@
+import day3
+
+def test_phase1():
+    assert day3.phase1('>') == 2
+    assert day3.phase1('^>v<') == 4
+    assert day3.phase1('^v^v^v^v^v') == 2
+
+def test_phase2():
+    assert day3.phase2('^v') == 3
+    assert day3.phase2('^>v<') == 3
+    assert day3.phase2('^v^v^v^v^v') == 11
